d1cf1f792f0c304bd110ee37bc2418e5d090752a
Levi Szamek <levi@szamek.de>
2024-06-06 14:06:59 +0200

    WIP

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,24 +2,30 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 import { MapDataService } from '../map-data.service';
 
+interface CustomMarker {
+  color: string;
+  name: string;
+  marker: L.Marker;
+}
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit, AfterViewInit {
-  private map!: L.Map
-  markers: L.Marker[] = [
-    L.marker([49.41032, 8.69707]),
-    L.marker([49.4233, 8.69455]),
-    L.marker([49.4192882, 8.6931570]),
-    L.marker([49.4121420, 8.7092182]),
-    L.marker([49.4035109, 8.7269402]),
-    L.marker([49.4023483, 8.6855940]),
-    L.marker([49.4046826, 8.6760974]),
-    L.marker([49.4092882, 8.6931570]),
-    L.marker([49.4281053, 8.6860676]),
-    L.marker([49.4167849, 8.6676842]), // Zentralmensa
+  private map!: L.Map;
+  markers: CustomMarker[] = [
+    { color: 'red', name: 'Marker 1', marker: L.marker([49.41032, 8.69707]) },
+    { color: 'red', name: 'Marker 2', marker: L.marker([49.4233, 8.69455]) },
+    { color: 'red', name: 'Marker 3', marker: L.marker([49.4192882, 8.6931570]) },
+    { color: 'blue', name: 'Marker 4', marker: L.marker([49.4121420, 8.7092182]) },
+    { color: 'blue', name: 'Marker 5', marker: L.marker([49.4035109, 8.7269402]) },
+    { color: 'blue', name: 'Marker 6', marker: L.marker([49.4023483, 8.6855940]) },
+    { color: 'blue', name: 'Marker 7', marker: L.marker([49.4046826, 8.6760974]) },
+    { color: 'blue', name: 'Marker 8', marker: L.marker([49.4092882, 8.6931570]) },
+    { color: 'blue', name: 'Marker 9', marker: L.marker([49.4281053, 8.6860676]) },
+    { color: 'green', name: 'Zentralmensa', marker: L.marker([49.4167849, 8.6676842]) }, // Zentralmensa
   ];
 
   constructor(private mapDataService: MapDataService) { }
@@ -40,61 +46,25 @@ export class MapComponent implements OnInit, AfterViewInit {
   }
 
   private addMarkers(): void {
-    // Define arrays for markers
-    const redMarkers = [this.markers[0], this.markers[1], this.markers[2]];
-    const blueMarkers = [this.markers[3], this.markers[4], this.markers[5], this.markers[6], this.markers[7], this.markers[8]];
-    const greenMarkers = [this.markers[9]];
-
-    // Add red markers with custom red icon
-    redMarkers.forEach((marker, index) => {
+    this.markers.forEach((customMarker, index) => {
+      const marker = customMarker.marker;
       marker.on('click', () => this.handleMarkerClick(index));
-      const redIcon = new L.Icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+      const iconUrl = `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${customMarker.color}.png`;
+      const customIcon = new L.Icon({
+        iconUrl: iconUrl,
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
         shadowSize: [41, 41],
       });
-
-      marker.setIcon(redIcon);
-      marker.addTo(this.map);
-    });
-
-    // Add blue markers with custom blue icon
-    blueMarkers.forEach((marker, index) => {
-      marker.on('click', () => this.handleMarkerClick(index + redMarkers.length));
-      const blueIcon = new L.Icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
-        shadowSize: [41, 41],
-      });
-
-      marker.setIcon(blueIcon);
-      marker.addTo(this.map);
-    });
-
-    greenMarkers.forEach((marker, index) => {
-      marker.on('click', () => this.handleMarkerClick(index + redMarkers.length + blueMarkers.length));
-      const greenIcon = new L.Icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
-        shadowSize: [41, 41],
-      });
-
-      marker.setIcon(greenIcon);
+      marker.setIcon(customIcon);
       marker.addTo(this.map);
     });
   }
 
   private centerMap() {
-    const bounds = L.latLngBounds(this.markers.map(marker => marker.getLatLng()));
+    const bounds = L.latLngBounds(this.markers.map(marker => marker.marker.getLatLng()));
     this.map.fitBounds(bounds);
   }
 

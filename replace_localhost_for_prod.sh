a4fe3b15e2cc7b780a8e4a12ba5726f5a0bb7066
Levi <levi@szamek.de>
2025-06-04 18:30:58 +0000

    fix: replace to use https

--- replace_localhost_for_prod.sh
+++ replace_localhost_for_prod.sh
@@ -1,17 +1,18 @@
-
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 file.json replacement_domain"
+  echo "Example: $0 config.json heidelberg-hoeren.de"
   exit 1
 fi
 
 FILE="$1"
 REPLACEMENT="$2"
 
-jq --arg rep "$REPLACEMENT" '
+jq --arg rep "https://$REPLACEMENT" '
   walk(
     if type == "string" then
+      gsub("http://localhost(:[0-9]+)?"; $rep) |
       gsub("localhost(:[0-9]+)?"; $rep)
     else
       .
@@ -19,4 +20,4 @@ jq --arg rep "$REPLACEMENT" '
   )
 ' "$FILE" > tmp.json && mv tmp.json "$FILE"
 
-echo "Replaced 'localhost[:port]' with '$REPLACEMENT' in $FILE"
+echo "Replaced localhost[:port] with 'https://$REPLACEMENT' in $FILE"

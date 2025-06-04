af2b947f567769ab9d8be26473cd30e6f072e9c7
Levi <levi@szamek.de>
2025-06-04 14:36:44 +0000

    feat: add script to make paths production ready

---
+++ replace_localhost_for_prod.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 file.json replacement_domain"
+  exit 1
+fi
+
+FILE="$1"
+REPLACEMENT="$2"
+
+jq --arg rep "$REPLACEMENT" '
+  walk(
+    if type == "string" then
+      gsub("localhost"; $rep)
+    else
+      .
+    end
+  )
+' "$FILE" > tmp.json && mv tmp.json "$FILE"
+
+echo "Replaced 'localhost' with '$REPLACEMENT' in $FILE"
